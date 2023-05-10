d8051bd56fe3e878347590891d44a5eeb307caf6
Aaron McCollum <59707347+aaronmccollum@users.noreply.github.com>
2023-05-09 20:48:06 -0400

    Create numberGuesser.js
    Copy+paste of project code from Number Guesser on Codecademy

---
+++ numberGuesser.js
@@ -0,0 +1,33 @@
+// Partial project code for Number Guesser project on Codecademy
+// Other part of project code provided by Codecademy on their website
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+// function to generate a random number
+const generateTarget = () => {
+  return Math.floor(Math.random() * 9) + 1;
+};
+
+const compareGuesses = (humanGuess, computerGuess, secretNum) => {
+  // get the absolute value of each player's differences
+  const humanGuessDistance = Math.abs(humanGuess - secretNum);
+  const computerGuessDistance = Math.abs(computerGuess - secretNum);
+
+  // return 'true' if human guess is equal or lower than computer
+  return humanGuessDistance <= computerGuessDistance ? true : false;
+}
+
+// updates score by 1 depending on who won
+const updateScore = winnerStr => {
+  if (winnerStr === 'human') { humanScore += 1; };
+  if (winnerStr === 'computer') { computerScore += 1; };
+}
+
+// function that advances the round number of the game
+const advanceRound = () => {
+  currentRoundNumber++;
+}
